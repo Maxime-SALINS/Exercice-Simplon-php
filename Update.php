5ddd2e5badf99b986b53f276711d56081a412c18
Maxime <139247550+Maxime-SALINS@users.noreply.github.com>
2024-02-21 15:18:07 +0100

    End update data

--- Update.php
+++ Update.php
@@ -7,21 +7,38 @@
 </form>
 
 <?php
-session_start();
+require_once 'data.php';
 
 $id = $_GET['id'];
+
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $vendu= $_POST['vendu'];
-    $stock= $_POST['stock'];
-    $newInput = array("vendu" => $vendu, "stock"=> $stock);
     
-    foreach ($_SESSION['cars'] as $car){
-        if ($car['id'] == $id) {
-            $index = array_search($car, $_SESSION['cars']);
-            $_SESSION['cars'][$index]= array_replace($car, $newInput);
-            header('Location:index.php');   
-        }
-    };
+    $vendu= intval($_POST['vendu']);
+    $stock= intval($_POST['stock']);
+    
+    //On prépare la requête SQL UPDATE
+    $reqprapare = $conn->prepare("UPDATE cars SET stock = :stock, vendu = :vendu WHERE id = :id");
+
+    //On Lie les variables $vendu et $stock à :vendu et :stock et $id à :id
+    $reqprapare->bindValue(':id', $id);
+    $reqprapare->bindValue(':stock', $stock);
+    $reqprapare->bindValue(':vendu', $vendu);
+
+    //On execute la requête
+    $update = $reqprapare->execute();
+
+    //On se redirige vers la page index.php
+    header('Location:index.php');
+
+    //Ancien Exercice
+        // foreach ($_SESSION['cars'] as $car){
+        //     if ($car['id'] == $id) {
+        //         $index = array_search($car, $_SESSION['cars']);
+        //         $_SESSION['cars'][$index]= array_replace($car, $newInput);
+        //         header('Location:index.php');   
+        //     }
+        // };
+    //
 };
 
 // var_dump($_SESSION['cars']);
