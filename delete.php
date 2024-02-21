e47c3ed6acdc5ca482bee8e585d908903dd59cf8
Maxime <139247550+Maxime-SALINS@users.noreply.github.com>
2024-02-21 15:27:11 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -3,27 +3,24 @@ require_once 'data.php';
 require_once './partials/header.php';
 
 $id = $_GET["id"];
+//On prépare la requête SQL DELETE
+$reqprepare = $conn->prepare("DELETE FROM `cars` WHERE id = :id");
 
-foreach ($table as $car) {
-    if ($car["id"] == $id) {
-        
-        //On prépare la requête SQL DELETE
-        $reqprepare = $conn->prepare("DELETE FROM `cars` WHERE id = :id");
+//On Lie la variable $id à 'id'
+$reqprepare->bindValue(':id', $id);
 
-        //On Lie la variable $id à 'id'
-        $reqprepare->bindValue(':id', $id);
+//On execute la requête
+$delete = $reqprepare->execute();
 
-        //On execute la requête
-        $delete = $reqprepare->execute();
+//On redirige vers la page index.php
+header('Location:index.php');
+// echo "la suppression à marcher";
 
-        echo "la suppression à marcher";
-
-        //Ancien exercice
-            // $index = array_search($car, $_SESSION['cars']);
-            // array_splice($_SESSION['cars'], $index , 1);
-            // var_dump($_SESSION['cars']);
-        //
-
-        header('Location:index.php');
-    }
-}
+//Ancien exercice
+// foreach ($table as $car) {
+//     if ($car["id"] == $id) {
+//         $index = array_search($car, $_SESSION['cars']);
+//         array_splice($_SESSION['cars'], $index , 1);
+//         var_dump($_SESSION['cars']);     
+//     }
+// }
