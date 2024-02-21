3373b767cf8ed66ab83001bb9f9ab2e5217fdf6b
Maxime <139247550+Maxime-SALINS@users.noreply.github.com>
2024-02-21 14:51:59 +0100

    End of DELETE

--- delete.php
+++ delete.php
@@ -2,17 +2,28 @@
 require_once 'data.php';
 require_once './partials/header.php';
 
-
 $id = $_GET["id"];
 
-foreach ($table as $car){
+foreach ($table as $car) {
     if ($car["id"] == $id) {
         
-        // $index = array_search($car, $_SESSION['cars']);
-        // array_splice($_SESSION['cars'], $index , 1);
-        // var_dump($_SESSION['cars']);
+        //On prépare la requête SQL DELETE
+        $reqprepare = $conn->prepare("DELETE FROM `cars` WHERE id = :id");
+
+        //On Lie la variable $id à 'id'
+        $reqprepare->bindValue(':id', $id);
+
+        //On execute la requête
+        $delete = $reqprepare->execute();
+
+        echo "la suppression à marcher";
+
+        //Ancien exercice
+            // $index = array_search($car, $_SESSION['cars']);
+            // array_splice($_SESSION['cars'], $index , 1);
+            // var_dump($_SESSION['cars']);
+        //
+
         header('Location:index.php');
     }
 }
-
-
