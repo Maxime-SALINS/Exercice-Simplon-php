a25208fbc8a610a17c90c9615eed1761c7b00844
Maxime <139247550+Maxime-SALINS@users.noreply.github.com>
2024-03-19 12:34:04 +0100

    Exercice OBJ USER

--- Exercice POO/index.php
+++ Exercice POO/index.php
@@ -6,19 +6,77 @@
 
 // var_dump(get_declared_classes());
 
-class Ballon {
+// class Ballon {
 
-    public $marque;
-    public $sport;
+//     public $marque;
+//     public $sport;
 
-    function lancer(){
-        echo "Vous avez lancer le ballon";
+//     function lancer(){
+//         echo "Vous avez lancer le ballon";
+//     }
+// }
+
+// $ballonfoot = new Ballon();
+
+// $ballonfoot -> marque = "Nike";
+// $ballonfoot -> sport = "foot";
+
+// var_dump($ballonfoot);
+
+class User
+{
+    public $name;
+    public $age;
+    public $avatar;
+
+    public function choixAvatar($param)
+    {
+        $table = ['😊', '😒', '😎', '🙄'];
+        $this -> avatar = $table[$param];
+    }
+
+    public function tchater($message)
+    {
+        echo "- $this -> avatar | $this -> name : $message \n";
+    }
+
+    public function viellir($param)
+    {
+        $this->age = $this->age + $param;
+        echo "Wow ! à $this->age il est temps de crever $this->name !";
     }
 }
 
-$ballonfoot = new Ballon();
+//Michel
+$michel = new User();
+$michel->age = 54;
+$michel->name = "Michel";
+$michel->choixAvatar(2);
 
-$ballonfoot -> marque = "Nike";
-$ballonfoot -> sport = "foot";
+//David
+$david = new User();
+$david->age = 26;
+$david->name = "David";
+$david->choixAvatar(1);
 
-var_dump($ballonfoot);
+//Zack
+$zack = new User();
+$zack->age = 25;
+$zack->name = "Zack";
+$zack->choixAvatar(0);
+//Samy
+$samy = new User();
+$samy->age = 30;
+$samy->name = "Samy";
+$samy->choixAvatar(3);
+
+//tchat
+
+$michel->tchater("Salut la compagnie !");
+$david->tchater("Shalom sha va !");
+$zack->tchater("Salam, ça va, David Bordus ?");
+$samy->tchater("Qu'est ce que je fou la moi !");
+
+echo $zack->age . " ans \n";
+
+$zack->viellir(10);
